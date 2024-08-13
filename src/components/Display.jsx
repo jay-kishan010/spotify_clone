4833042b2e8f2c02b3cb4e3d3005d88fe7a5fdcc
jay-kishan010 <124438245+jay-kishan010@users.noreply.github.com>
2024-08-13 22:45:57 +0530

    ..

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,12 +1,35 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useLocation } from "react-router-dom"
 import DisplayHome from "./DisplayHome"
+import { DisplayAlbum } from "./DisplayAlbum"
+import { useEffect, useRef } from "react"
+import { albumsData } from "../assets/assets"
 
 
 const Display = () => {
+
+  const displayRef=useRef();
+  
+  const location=useLocation();
+  const isAlbum=location.pathname.includes("album");
+  // console.log(location.pathname.slice(-1))
+  // console.log(isAlbum)
+  const albumId=isAlbum ? location.pathname.slice(-1):"";
+  const bgColor=albumsData[Number(albumId)].bgColor;
+  // console.log(bgColor)
+
+  useEffect(()=>{
+    if(isAlbum){
+      displayRef.current.style.background='linear-gradient(${bgColor},#121212)'
+    }
+    else{
+      displayRef.current.style.background='linear-gradient(#121212,#121212)'
+    }
+  })
   return (
    <div className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0">
     <Routes>
       <Route path="/" element={<DisplayHome/>}/>
+      <Route path="/album/:id" element={<DisplayAlbum/>}/>
     </Routes>
    </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,47 @@
-import { assets } from "../assets/assets"
+import { useNavigate } from "react-router-dom";
+import { assets } from "../assets/assets";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
   return (
-   <>
-  <div className="w-full flex justify-between items-center font-semibold">
-    <div className="flex items-center gap-2">
-        <img src={assets.arrow_left} alt="" className="w-8 bg-black p-2 rounded-2xl cursor-pointer" />
-        <img src={assets.arrow_right} alt="" className="w-8 bg-black p-2 rounded-2xl cursor-pointer" />
-    </div>
-    <div className="flex items-center gap-4">
-        <p className="bg-white text-black text-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer">Explore Premium</p>
- <p className="bg-black py-1 px-3 rounded-2xl text-[15px] cursor-pointer text-white">Install App</p>
-  <p className="bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center">D</p>
-    </div>
+    <>
+      <div className="w-full flex justify-between items-center font-semibold">
+        <div className="flex items-center gap-2">
+          <img onClick={()=>navigate(-1)}
+            src={assets.arrow_left}
+            alt=""
+            className="w-8 bg-black p-2 rounded-2xl cursor-pointer"
+          />
+          <img onClick={()=>navigate(1)}
+            src={assets.arrow_right}
+            alt=""
+            className="w-8 bg-black p-2 rounded-2xl cursor-pointer"
+          />
+        </div>
+        <div className="flex items-center gap-4">
+          <p className="bg-white text-black text-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer">
+            Explore Premium
+          </p>
+          <p className="bg-black py-1 px-3 rounded-2xl text-[15px] cursor-pointer text-white">
+            Install App
+          </p>
+          <p className="bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center">
+            D
+          </p>
+        </div>
+      </div>
+      <div className="flex items-center gap-2 mt-4">
+        <p className="bg-white text-black cursor-pointer px-4 py-1 rounded-2xl">
+          All
+        </p>
+        <p className="bg-black px-4 py-1 cursor-pointer rounded-2xl">Music</p>
+        <p className="bg-black px-4 py-1 cursor-pointer rounded-2xl">
+          Podcasts
+        </p>
+      </div>
+    </>
+  );
+};
 
-  </div>
-<div className="flex items-center gap-2 mt-4">
-    <p className="bg-white text-black cursor-pointer px-4 py-1 rounded-2xl">All</p>
-    <p className="bg-black px-4 py-1 cursor-pointer rounded-2xl">Music</p>
-    <p className="bg-black px-4 py-1 cursor-pointer rounded-2xl">Podcasts</p>
-</div>
-
-   </>
-  )
-}
-
-export default Navbar
+export default Navbar;
